57643b14b578f2ebc49f58b98c15856dbd333816
Terje Sandstrom <terje@osiris.no>
2013-11-11 23:03:50 +0100

    Added in a NUNit test in Mstest, renamed to maintain good naming

---
+++ OldTestCode/MSTest/NUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MSTest
+{
+    [TestFixture]
+    class NUnitTest
+    {
+        [Test]
+        public void NTestit1()
+        {
+            Assert.IsTrue(true);
+        }
+
+    }
+}
