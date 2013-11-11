60267d3a417abd210a641991e283c148cbb3f7c0
Terje Sandstrom <terje@osiris.no>
2013-11-11 22:40:42 +0100

    Added a XUnit test

---
+++ OldTestCode/XUnitTests/XTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTests
+{
+    public class XTests
+    {
+
+        [Fact]
+        public void XTestit()
+        {
+            Assert.True(true);
+        }
+    }
+}
