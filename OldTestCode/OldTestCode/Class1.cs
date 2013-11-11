e0b7e50a44506beefb89aa4f41ba5191297d2fb4
Terje Sandstrom <terje@osiris.no>
2013-11-11 21:18:48 +0100

    Step 1:  Added two MSTests, which is old legacy code

--- OldTestCode/OldTestCode/Class1.cs
+++ OldTestCode/OldTestCode/Class1.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace OldTestCode
 {
-    public class Class1
+    [TestClass]
+    public class SomeOldTest
     {
+        [TestMethod]
+        public void Testit()
+        {
+            Assert.IsTrue(true);
+        }
     }
 }
