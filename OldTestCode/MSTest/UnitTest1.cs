ff9d691e74cbb04374cdad429a672fc7017898e4
Terje Sandstrom <terje@osiris.no>
2013-11-11 23:16:33 +0100

    Added mixin test with testcase data

--- OldTestCode/MSTest/UnitTest1.cs
+++ OldTestCode/MSTest/UnitTest1.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NU = NUnit.Framework;
+using TestFixture = NUnit.Framework.TestFixtureAttribute;
+using Test = NUnit.Framework.TestAttribute;
+using TestCase = NUnit.Framework.TestCaseAttribute;
 
 namespace MSTest
 {
+    [TestFixture]
     [TestClass]
     public class MsTest1
     {
@@ -19,11 +24,20 @@ namespace MSTest
             Assert.IsTrue(true);
         }
 
-
+        
         [TestMethod]
         public void MsTestMethod3()
         {
             Assert.IsTrue(true);
+        }
+
+        [TestCase(4,8)]
+        [TestCase(2,4)]
+        [Test]
+        public void NUnitDataDrivenTest(int x,int y)
+        {
+            int res = x*2;
+            NU.Assert.That(res,NU.Is.EqualTo(y));
         }
 
 
