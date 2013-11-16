5d1284586d6cd018d8617f336069ac47bdcc25eb
Terje Sandstrom <terje@osiris.no>
2013-11-16 18:34:55 +0100

    added script for getting down the brances

---
+++ getbranches.cmd
@@ -0,0 +1,18 @@
+git checkout -b EightAttributes origin/EightAttributes
+git checkout -b FifthLegacy1 origin/FifthLegacy1
+git checkout -b FifthMsTestLegacy origin/FifthMsTestLegacy
+git checkout -b FirstTest origin/FirstTest
+git checkout -b FourthWithXUnit origin/FourthWithXUnit
+git checkout -b NineBXUnitMixin origin/NineBXUnitMixin
+git checkout -b NineCNunitTheoryExperiment origin/NineCNunitTheoryExperiment
+git checkout -b NineXUNitMixin origin/NineXUNitMixin
+git checkout -b SecondNUnitTest origin/SecondNUnitTest
+git checkout -b SevenMixin origin/SevenMixin
+git checkout -b SixthXUnitTestLegacy origin/SixthXUnitTestLegacy
+git checkout -b Step11-MassiveMSTests origin/Step11-MassiveMSTests
+git checkout -b Step11B-DifferentInfo origin/Step11B-DifferentInfo
+git checkout -b Step11B-DifferentInfo-NUnit origin/Step11B-DifferentInfo-NUnit
+git checkout -b TenB origin/TenB
+ git checkout -b TenXUnitTheory origin/TenXUnitTheory
+git checkout -b ThirdWithoutAdapter origin/ThirdWithoutAdapter
+
