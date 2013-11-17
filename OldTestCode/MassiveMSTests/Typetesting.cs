514d31a0b03258532b19ae14a52947543f7b4990
Terje Sandstrom <terje@osiris.no>
2013-11-17 09:23:30 -0800

    Renamed class, added typetesting

---
+++ OldTestCode/MassiveMSTests/Typetesting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = NUnit.Framework.AssertMs;
+
+
+namespace MassiveTests2
+{
+
+    public class AnotherHugeMSTestClassWithSomeInstanceTests
+    {
+
+        [TestMethod]
+        public void MSTestit799()
+        {
+            var d = DateTime.Now;
+            Assert.IsInstanceOfType(d, typeof(DateTime));
+        }
+
+
+        [TestMethod]
+        public void MSTestit899()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+
+}
+
